21aa0f38fbc50aa4359e563b8a329606035af70d
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-05-12 09:18:02 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,38 @@
+basic.showIcon(IconNames.Angry)
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) < -100) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.Y) > 100) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) < -100) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) > 100) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+})
